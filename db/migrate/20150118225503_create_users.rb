6939b778e7540ee0373ad3c8379a30dd6435ab1a
Linda Gibson <dakidz@live.com>
2015-01-18 14:59:09 -0800

    create users table

---
+++ db/migrate/20150118225503_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users, force: true do |t|
+      t.string "name"
+      t.string "email"
+      t.string "password_digest"
+    end
+  end
+end
